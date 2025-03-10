7fea3fff8751d6a798d50b364d25db1102c0e25b
NikitaPoraiyar <poraiyarnikita@gmail.com>
2025-03-10 23:14:32 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/",  
   build: {
-    outDir: 'dist'
+    outDir: "dist"
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'https://job-finder-l5ed.onrender.com',
-        changeOrigin: true,
-        secure: false
-      }
+      "/api": "https://job-finder-l5ed.onrender.com"
     }
   }
 });
