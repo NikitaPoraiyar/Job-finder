f93b1ae459b5927f3e8c2e540ce5527b478cbec7
NikitaPoraiyar <poraiyarnikita@gmail.com>
2025-03-11 00:26:38 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: '/',
   build: {
-    outDir: "dist"
+    outDir: 'dist',
   },
   server: {
-    proxy: {
-      "/api": {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
-        secure: false
-      }
-    }
+    historyApiFallback: true,
   }
 });
