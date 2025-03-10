d69d4b4db94fef11e8611d2b76f317bce52ac6cd
NikitaPoraiyar <poraiyarnikita@gmail.com>
2025-03-10 21:51:08 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -22,10 +22,14 @@ app.use('/api/jobs', jobRoutes);
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("Connecting to MongoDB");
     } catch(error) {
         console.log("Error connecting to MongoDB:", error);
+        process.exit(1);
     }
 };
 
